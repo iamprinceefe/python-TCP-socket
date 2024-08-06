fb071a9c18c979c8a7f121eeb9f2e90dab083bb2
Prince Efe Oruma <156837295+iamprinceefe@users.noreply.github.com>
2024-08-06 16:35:29 -0400

    Add files via upload

---
+++ Client.py
@@ -0,0 +1,19 @@
+#!/usr?bin/python3
+
+import socket
+
+clientsocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+host = socket.gethostname()
+
+port = 444
+
+clientsocket.connect(('host', port))
+
+message = clientsocket.recv(1023)
+
+
+clientsocket.close()
+
+print(message.decode('ascii'))
+                     
